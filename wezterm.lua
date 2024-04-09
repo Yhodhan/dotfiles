697ee0ac5d0a2992a8423bd85fba690d66998006
hixion <mscuderi855@gmail.com>
2024-04-09 00:49:35 -0300

    Upload wezterm config

---
+++ wezterm.lua
@@ -0,0 +1,42 @@
+local wezterm = require("wezterm")
+
+return {
+  default_prog = {"nu"},
+  color_scheme = "Urple (Gogh)",
+  window_background_opacity = 0.6,
+  enable_tab_bar = false,
+  window_padding = {
+    left = 0.5,
+    right = 0.5,
+    top = 0.5,
+    bottom = 0.5,
+  },
+  window_decorations = "NONE",
+  keys = {
+    -- {
+    --   key = "RightArrow",
+    --   mods = "ALT",
+    --   action = wezterm.action.
+    -- }
+    {
+      key = "v",
+      mods = "CTRL",
+      action = wezterm.action.SplitVertical {domain = 'CurrentPaneDomain'}
+    },
+    {
+      key = "h",
+      mods = "CTRL",
+      action = wezterm.action.SplitHorizontal {domain = 'CurrentPaneDomain'}
+    },
+    {
+      key = "h",
+      mods = "ALT",
+      action = wezterm.action.ActivatePaneDirection 'Left'
+    },
+    {
+      key = "l",
+      mods = "ALT",
+      action = wezterm.action.ActivatePaneDirection 'Right'
+    }
+  }
+}
