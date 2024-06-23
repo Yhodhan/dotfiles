5b33a776f69338b86f23e3d94d9b000045a64b25
Yhodhan <mscuderi855@gmail.com>
2024-06-23 00:01:22 -0300

    Improve doom emacs config

---
+++ doom.d/.doom.d/custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(evil-tutor-sc evil-tutor-ja cyberpunk-theme cyberpunk-2019-theme cybercafe-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(setq confirm-kill-emacs nil)
