c92907a7ea7ea5b76bdb14ee52b23182eba0300f
Yhodhan <mscuderi855@gmail.com>
2024-07-23 21:38:21 -0300

    Update fish config to use modern tools as defaults

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+zoxide init fish --cmd cd | source
+status --is-interactive; and rbenv init - fish | source
+
+source /home/linuxbrew/.linuxbrew/opt/asdf/libexec/asdf.fish
